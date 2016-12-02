52afae5ab3333cb3935380272531e4a937599ecf
Harry <harry_w@foxmail.com>
2016-12-02 14:31:37 +0800

    .

--- src/Harry.Logging/Common/Throw.cs
+++ src/Harry.Logging/Common/Throw.cs
@@ -12,15 +12,12 @@ namespace Harry.Logging.Common
         /// <param name="exceptions"></param>
         public static Exception MergeExceptions(IEnumerable<Exception> exceptions)
         {
-            StringBuilder sb = new StringBuilder(512);
+            StringBuilder sb = new StringBuilder(1024);
             foreach (var item in exceptions)
             {
-                sb.Append(item.Message
-                    + Environment.NewLine
-                    + item.StackTrace
-                    + Environment.NewLine
-                    + "----------------------------------------------------"
-                    + Environment.NewLine);
+                sb.AppendLine(item.Message);
+                sb.AppendLine(item.StackTrace);
+                sb.AppendLine("----------------------------------------------------");
             }
             return new Exception(sb.ToString());
         }
